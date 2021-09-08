6a50a95758862e6fd88bba380dd2c6bbaa005f4e
Ariel Gerstein <ariel.gers@gmail.com>
2021-09-08 19:47:13 -0300

    Add eslint + prettier config

---
+++ eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: ["@typescript-eslint", "prettier"],
+  env: {
+    browser: true,
+    es6: true,
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  rules: {
+    "prettier/prettier": ["error"],
+  },
+};
